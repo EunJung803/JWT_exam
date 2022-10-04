0ded012c847e5c1aac3b0d5d063150a10fc11aa0
EunJung803 <aimee3322@gmail.com>
2022-10-04 14:00:34 +0900

    Setting

---
+++ src/test/java/com/ll/exam/JWT_exam/JwtExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.JWT_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
