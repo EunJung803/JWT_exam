03bb7897bb4b92cad3f1c32d37713be92d3802f7
EunJung803 <aimee3322@gmail.com>
2022-10-04 18:11:55 +0900

    Feat : AccessToken 생성

--- src/main/java/com/ll/exam/JWT_exam/JwtExamApplication.java
+++ src/main/java/com/ll/exam/JWT_exam/JwtExamApplication.java
@@ -1,7 +1,9 @@
 package com.ll.exam.JWT_exam;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JwtExamApplication {
@@ -10,4 +12,8 @@ public class JwtExamApplication {
 		SpringApplication.run(JwtExamApplication.class, args);
 	}
 
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper();
+	}
 }
